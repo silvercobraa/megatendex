34d23f3b176c702f6dd2adf376a45476f0616161
silvercobraa <silver.cobra1n@gmail.com>
2020-09-15 17:55:09 -0300

    Updates

---
+++ precache-manifest.7ccf6750a7e8d0397beb420e316c9e68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f388ee43bc61a0ae2934a11351079f",
+    "url": "/megatendex/index.html"
+  },
+  {
+    "revision": "4930c58b3b09f35001e3",
+    "url": "/megatendex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d14384ac284d088d5092",
+    "url": "/megatendex/static/js/2.000b6f32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/megatendex/static/js/2.000b6f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4930c58b3b09f35001e3",
+    "url": "/megatendex/static/js/main.c38e638a.chunk.js"
+  },
+  {
+    "revision": "fb33944c6e455afe4d24",
+    "url": "/megatendex/static/js/runtime-main.f55a2324.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/megatendex/static/media/logo.5d5d9eef.svg"
+  }
+]);
